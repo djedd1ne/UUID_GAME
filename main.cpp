7940dd08235ccb99fdd9de1e5da1a195174fc361
Djallal Mekki <djmekki@2-A-4.42heilbronn.de>
2024-05-20 14:36:41 +0200

    Generate 100 random uuids convert uuids to string and print

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_io.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <list>
 
-boost::uuids::uuid generate() {
-    boost::uuids::uuid uuid;
-    std::cout << boost::uuids::to_string(uuid) <<std::endl;
-    return uuid;
+std::list<boost::uuids::uuid> generate() {
+    std::list<boost::uuids::uuid> uuids;
+    boost::uuids::random_generator gen;
+    for (int i = 0; i < 100; ++i) {
+        boost::uuids::uuid new_uuid = gen();
+        uuids.push_back(new_uuid);
+        std::cout << "Generated UUID: " << boost::uuids::to_string(new_uuid) << std::endl;
+    }
+    return uuids;
 }
 
 int main(){
-    boost::uuids::uuid uuid = generate();
+    std::list<boost::uuids::uuid> uuids = generate();
     return 0;
 }
