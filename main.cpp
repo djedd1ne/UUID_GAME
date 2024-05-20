7d210ff00ce7b77525e41878fedbf50231879260
Djallal Mekki <djmekki@2-A-4.42heilbronn.de>
2024-05-20 14:44:23 +0200

    Add selectRandomCharacter() seed random generator using srand generate random index to select random character

--- main.cpp
+++ main.cpp
@@ -10,12 +10,21 @@ std::list<boost::uuids::uuid> generate() {
     for (int i = 0; i < 100; ++i) {
         boost::uuids::uuid new_uuid = gen();
         uuids.push_back(new_uuid);
-        std::cout << "Generated UUID: " << boost::uuids::to_string(new_uuid) << std::endl;
     }
     return uuids;
 }
 
+char selectRandomCharacter() {
+    const std::string characters = "0123456789abcdef";
+    int index = std::rand() % 16; // Generate a random index [0, 15]
+    return characters[index];
+}
+
 int main(){
+    std::srand(static_cast<unsigned int>(std::time(nullptr))); // seed random generator
     std::list<boost::uuids::uuid> uuids = generate();
+    char randomChar = selectRandomCharacter();
+    std::cout << "Random character: " << randomChar << std::endl; // print random character
+
     return 0;
 }
